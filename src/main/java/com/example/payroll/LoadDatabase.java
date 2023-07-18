98bcf7b0ad3507e91cdf0d8158575fbaf3ad2ef4
lb-Michael <michael.kessler@livingbrain.de>
2023-07-18 12:15:55 +0200

    finish project

--- src/main/java/com/example/payroll/LoadDatabase.java
+++ src/main/java/com/example/payroll/LoadDatabase.java
@@ -12,10 +12,17 @@ class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins" , "Ring bearer")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Frodo", "Baggins" , "Ring bearer")));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preload " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("Samsung s21", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preload " + order));
         };
     }
 }
--- src/main/java/com/example/payroll/Order.java
+++ src/main/java/com/example/payroll/Order.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.transaction.Status;
 
 @Entity
 @Table(name = "CUSTOMER_ORDER")
